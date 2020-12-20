ee45f86d393f240b09451f4b3f650122d1baeb9a
Ehtesham <mohd.ehtesh@gmail.com>
2020-12-20 09:25:35 +0530

    File Upload Database

--- models/book.js
+++ models/book.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const path = require('path')
-
-const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
   title: {
@@ -24,7 +21,11 @@ const bookSchema = new mongoose.Schema({
     required: true,
     default: Date.now
   },
-  coverImageName: {
+  coverImage: {
+    type: Buffer,
+    required: true
+  },
+  coverImageType: {
     type: String,
     required: true
   },
@@ -36,16 +37,16 @@ const bookSchema = new mongoose.Schema({
 })
 
 
-// this is a virtual property. It works exactly same as the properties above, except that it derives its value from the above properties.
-// Basically what this doing is, when we call 'coverImagePath' from books/index.ejs in order to view the uploaded image, it calls the get()
-// function below.
 bookSchema.virtual('coverImagePath').get(function() {
-  if (this.coverImageName != null) {
-    return path.join('/', coverImageBasePath, this.coverImageName)
-    // this address is returned to the 'src' attribute of html element '<img>' and it gets the image that we uploaded in that address
-    // through multer
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
   }
 })
 
+
+// this is a virtual property. It works exactly same as the properties above, except that it derives its value from the above properties.
+// Basically what this doing is, when we call 'coverImagePath' from books/index.ejs in order to view the uploaded image, it calls the get()
+// function above, i.e ".get(function(){"
+// 'return' of this function is taken by the 'src' attribute of html element '<img>' and it gets the image.
+
 module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
